fee64536a0f4129a4c2a01637c1f59c824ef9f87
Joergen-Lund <jorgen2817@gmail.com>
2021-09-15 15:18:13 +0200

    init

--- main.js
+++ main.js
@@ -14,9 +14,10 @@ draggables.forEach(draggable => {
 containers.forEach(container => {
     container.addEventListener('dragover', e => {
         e.preventDefault()
+
         const afterElement = getDragAfterElement(container, e.clientY)
-        console.log(afterElement)
         const draggable = document.querySelector('.dragging')
+
         if (afterElement == null) {
             container.appendChild(draggable)
         } else {
@@ -31,7 +32,6 @@ function getDragAfterElement(container, y) {
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect()
         const offset = y - box.top - box.height / 2
-        console.log(offset)
 
         if (offset < 0 && offset > closest.offset) {
             return { offset: offset, element: child }
